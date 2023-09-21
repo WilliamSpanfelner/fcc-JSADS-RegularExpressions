f2466e8180944d1cda4153b20f90412a9336adc2
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:17:14 +0300

    Challenge - start

--- 03_matchPossibleLiteralStrings.js
+++ 03_matchPossibleLiteralStrings.js
@@ -4,3 +4,9 @@
 /yes|no|maybe/
 
 // the above regex expressions will match more than one pattern
+
+/* Complete the regex petRegex to match the pets 
+dog, cat, bird, or fish. */
+let petString = "James has a pet cat.";
+let petRegex = /change/; // change this line
+let result = petRegex.test(petString);
