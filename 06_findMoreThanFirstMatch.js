62673b80cd4205be472bac72241c1fa71e3f3ef4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:58:20 +0300

    Extract multiple patterns with the g flag

--- 06_findMoreThanFirstMatch.js
+++ 06_findMoreThanFirstMatch.js
@@ -1,1 +1,10 @@
 // Find More Than the First Match
+/* the global flag g, will return an array of results
+corresponding to the number of times a word appears in 
+the string of interest */
+
+let testStr = "Repeat, Repeat, Repeat";
+let ourRegex = /Repeat/g;
+let result = testStr.match(ourRegex);
+
+console.log(result);  // [ 'Repeat', 'Repeat', 'Repeat' ]
