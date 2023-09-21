55a250059a99f36880913cae42507ab3c9c440a6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:20:22 +0300

    Challenge - solution

--- 03_matchPossibleLiteralStrings.js
+++ 03_matchPossibleLiteralStrings.js
@@ -1,12 +1,15 @@
 // Match a Literal String with Different Possibilities
 // Search for multiple patterns or strings with the | operator
-/yes|no/
-/yes|no|maybe/
+
+// /yes|no/
+// /yes|no|maybe/
 
 // the above regex expressions will match more than one pattern
 
 /* Complete the regex petRegex to match the pets 
 dog, cat, bird, or fish. */
 let petString = "James has a pet cat.";
-let petRegex = /change/; // change this line
+let petRegex = /dog|cat|bird|fish/; // change this line
 let result = petRegex.test(petString);
+
+console.log(result); // true
