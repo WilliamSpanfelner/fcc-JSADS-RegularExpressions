ed6a506ee3feba35872726ae7cc70bda406e035e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:42:42 +0300

    Add second .match() method example

--- 05_extractMatches.js
+++ 05_extractMatches.js
@@ -1,3 +1,9 @@
 // Extract Matches
 // The .match() method takes a regex and returns an array of matching words.
 console.log("Hello, World!".match(/Hello/));  // ["Hello"]
+
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+let result = ourStr.match(ourRegex);
+
+console.log(result);  // ["expressions"]
