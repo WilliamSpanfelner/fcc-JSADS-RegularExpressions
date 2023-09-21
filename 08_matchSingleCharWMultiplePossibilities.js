ea783782c81f1403c18518e85fde922fc0d33e2d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 18:37:09 +0300

    Match variations using character classes

--- 08_matchSingleCharWMultiplePossibilities.js
+++ 08_matchSingleCharWMultiplePossibilities.js
@@ -1,1 +1,23 @@
 // Match Single Character with Multiple Possibilities
+/* Character classes are placed inside square brackets
+and define a group of characters that you would like to
+match. */
+
+/* e.g. if it is desired to match big, bag and bug
+but not bog, one might create a regex as follows
+/b[iau]g/
+*/
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+let result = bigStr.match(bgRegex);  // ["big"]
+console.log(result);
+result = bagStr.match(bgRegex);  // ["bag"]
+console.log(result);
+result = bugStr.match(bgRegex);  // ["bug"]
+console.log(result);
+result = bogStr.match(bgRegex);  // null
+console.log(result);
