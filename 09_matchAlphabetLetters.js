27ddc76f6ac061b44c10e8c7c6fbec4fc7679b7b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 20:46:22 +0300

    Add regex with character range

--- 09_matchAlphabetLetters.js
+++ 09_matchAlphabetLetters.js
@@ -1,1 +1,15 @@
 // Match Letters of the Alphabet
+/* Match a range of characters using the - inside
+square brackets */
+
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+let result = catStr.match(bgRegex);  // ['cat']
+console.log(result);
+result = batStr.match(bgRegex);  // ['bat']
+console.log(result);
+result = matStr.match(bgRegex);  // null
+console.log(result);
+
