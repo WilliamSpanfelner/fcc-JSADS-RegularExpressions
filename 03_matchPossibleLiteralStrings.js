f6d72c73d41763c7e94ee385f4a838211fb91524
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:13:40 +0300

    Use the or operator to match multiple patterns

--- 03_matchPossibleLiteralStrings.js
+++ 03_matchPossibleLiteralStrings.js
@@ -1,1 +1,6 @@
 // Match a Literal String with Different Possibilities
+// Search for multiple patterns or strings with the | operator
+/yes|no/
+/yes|no|maybe/
+
+// the above regex expressions will match more than one pattern
