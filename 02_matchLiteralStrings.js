8edc65d8232da82f240aabca7862e6dfcd6df4a5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 14:48:30 +0300

    Challenge - start

--- 02_matchLiteralStrings.js
+++ 02_matchLiteralStrings.js
@@ -11,3 +11,12 @@ const wrongRegex = /kevin/;
 result = wrongRegex.test(testStr);
 
 console.log(result);  // false
+
+/* Complete the regex waldoRegex to find "Waldo" in the 
+string waldoIsHiding with a literal match */
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /search/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+console.log(result);
