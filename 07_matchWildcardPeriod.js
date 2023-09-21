79e750e6868f7adfaddba8a10d5ccf48f3b7d2cb
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 16:12:47 +0300

    Add wildcard character . example

--- 07_matchWildcardPeriod.js
+++ 07_matchWildcardPeriod.js
@@ -1,1 +1,11 @@
 // Match Anything with Wildcard Period
+// The wildcard character . will match any one character.
+
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+let result = huRegex.test(humStr);
+console.log(result);  // true
+
+result = huRegex.test(hugStr);
+console.log(result);  // true
