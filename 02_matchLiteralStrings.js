58da2f48b8778039e97a51d9a850fe11a2685ed3
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 14:45:16 +0300

    Attempt to match an alternative form of the regex

--- 02_matchLiteralStrings.js
+++ 02_matchLiteralStrings.js
@@ -1,6 +1,13 @@
 // Match Literal Strings
 const testStr = "Hello, my name is Kevin.";
 const testRegex = /Kevin/;
-const result = testRegex.test(testStr);
+let result = testRegex.test(testStr);
 
 console.log(result);  // true
+
+/* Note that the regex expression will not match other 
+forms of the word Kevin like kevin or KEVIN */
+const wrongRegex = /kevin/;
+result = wrongRegex.test(testStr);
+
+console.log(result);  // false
