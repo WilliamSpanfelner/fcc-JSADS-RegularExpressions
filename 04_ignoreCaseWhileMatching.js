658e42538439978fdd035f8da496c5b81bcdd85a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 15:28:06 +0300

    Challenge - start

--- 04_ignoreCaseWhileMatching.js
+++ 04_ignoreCaseWhileMatching.js
@@ -3,3 +3,13 @@
 will ignore case differeneces */
 
 //  /ignorecase/i
+
+/* Write a regex fccRegex to match freeCodeCamp, no 
+matter its case.  Your regex should not match any 
+abbreviations or variations with spaces.*/
+
+let myString = 'freeCodeCamp';
+let fccRegex = /change/;  // change this line
+let result = fccRegex.test(myString);
+
+console.log(result);
