9f4961d9426ce8c8899ce446f139a6ff7cc2279c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 20:58:30 +0300

    Match numbers and letters with regex

--- 10_matchNumbersAlphabet.js
+++ 10_matchNumbersAlphabet.js
@@ -1,1 +1,7 @@
 // Match Numbers and Letters of the Alphabet
+// The hyphen can be used to match numbers also
+
+let jennyStr = "Jenny8675309";
+let myRegex = /[a-z0-9]/ig;
+let result = jennyStr.match(myRegex);
+console.log(result);
