d675fdd83f30c1abfcafa354c667772bba942def
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 18:03:06 +0300

    Use \W in regex to exclude alphanumerics

--- 19_excludeLettersNumbers.js
+++ 19_excludeLettersNumbers.js
@@ -1,1 +1,13 @@
 // Match Everthing but Letters and Numbers
+/* The \W character class matches everything excluding 
+alphanumerics and numbers and is equivalent to:
+[^A-Za-z0-9_]
+*/
+
+let shortHand = /\W/;
+let numbers = "42%";
+let sentence = "Coding!";
+let result = numbers.match(shortHand);
+console.log(result);  // [ '%' ]
+result = sentence.match(shortHand);
+console.log(result);  // [ '!' ]
