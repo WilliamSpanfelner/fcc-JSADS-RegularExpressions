3b24f0fb91e08672a8f2dfc120c257ea50f1c596
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 17:27:02 +0300

    Test for alphanumerics with \w  character classs

--- 18_matchLettersNumbers.js
+++ 18_matchLettersNumbers.js
@@ -1,1 +1,21 @@
 // Match All letters and Numbers
+/* The \w character class is shorthand for 
+matching all numbers and upper & lowercase 
+letters of the alphabet.  The /w character 
+class also include the underscore character
+i.e. [A-Za-z0-9_] */
+
+let longHand = /[A-Za-z0-9_]+/;
+let shorthand = /\w+/;
+let numbers = '42';
+let varNames = 'important_var';
+let result = longHand.test(numbers); 
+console.log(result);  // true
+result = shorthand.test(numbers); 
+console.log(result);  // true
+result = longHand.test(varNames); 
+console.log(result);  // true
+result = shorthand.test(varNames); 
+console.log(result);  // true
+
+// \w is also known as a shorthand character class
