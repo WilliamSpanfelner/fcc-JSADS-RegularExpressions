3def9e8c2ab8943c2b2261f838eaf31d8b41ae72
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:08:43 +0300

    Challenge - start

--- 15_find1+Criminals.js
+++ 15_find1+Criminals.js
@@ -1,1 +1,8 @@
 // Find One or More Criminals in a Hunt
+/*
+Write a greedy regex that finds one or more criminals within
+a group of other people. A criminal is represented by the 
+capital letter C. */
+let myString = "P1P5P4CCCcP2P6P3";
+let myRegex = /./;
+let result = myString.match(myRegex);
