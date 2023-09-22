a6329f40e939f30065dffd8427275aa4e30317dd
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 18:50:54 +0300

    Challenge - solution

--- 21_matchAllNonNumbers.js
+++ 21_matchAllNonNumbers.js
@@ -5,7 +5,7 @@
 /* Use the shorthand character class for non-digits \D to 
 count how many non-digits are in movie titles. */
 let movieName = "2001: A Space Odyssey";
-let noNumRegex = /change/;  // Change this line
+let noNumRegex = /\D/g;  // Change this line
 let result = movieName.match(noNumRegex);
 
 console.log(noNumRegex, result, result.length);
