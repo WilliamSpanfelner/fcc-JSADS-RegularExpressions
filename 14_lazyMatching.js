3c65bb0f0f8cc445a056418b2c3363d567b1ea96
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 11:54:46 +0300

    Challenge - start

--- 14_lazyMatching.js
+++ 14_lazyMatching.js
@@ -16,3 +16,11 @@ console.log(result);  // [ 'titani' ]
 myRegex = /t[a-z]*?i/;
 result = myString.match(myRegex);
 console.log(result);  // [ 'ti' ]
+
+/* Fix teh regex /<.*>/ to return the HTML tag <h1> and 
+not the text "<h1>Winter is coming</h1>". Remember the
+wildcard . in a regular expression matches any character */
+
+let text = "<h1>Winter is coming</h1>";
+myRegex = /<.*>/;
+result = text.match(myRegex);
