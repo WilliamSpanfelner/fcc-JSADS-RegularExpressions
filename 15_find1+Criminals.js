1ec707733352bf566724eb5bc58328a5e489e3d4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:11:49 +0300

    Challenge - solution

--- 15_find1+Criminals.js
+++ 15_find1+Criminals.js
@@ -4,5 +4,6 @@ Write a greedy regex that finds one or more criminals within
 a group of other people. A criminal is represented by the 
 capital letter C. */
 let myString = "P1P5P4CCCcP2P6P3";
-let myRegex = /./;
+let myRegex = /C+/g;
 let result = myString.match(myRegex);
+console.log(result);
