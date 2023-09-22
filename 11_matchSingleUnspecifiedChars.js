05a51bbfd3f07f8cb53cc7854534b73ff87892e7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 10:40:53 +0300

    Challenge - start

--- 11_matchSingleUnspecifiedChars.js
+++ 11_matchSingleUnspecifiedChars.js
@@ -1,6 +1,14 @@
 // Match Single Characters Not Specified
 /* The ^ (caret character) is used to omit characters
 and should prefix the undesired characters */
+
 /*
 /[^aeiou]/gi will match all characters that are not a vowel
 */
+
+/* Create a single regex that matches all characters that are
+not a number or a vowel. Remember to include the appropriate
+flags in the regex. */
+let quoteSample = "3 blind mice.";
+let myRegex = /change/; // Change this line
+let result = myRegex;  // Change this line
