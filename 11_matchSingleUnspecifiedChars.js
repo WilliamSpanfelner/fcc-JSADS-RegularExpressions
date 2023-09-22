fd17b810bb6a19935cf1374a4a33774f2c9d1364
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 10:06:37 +0300

    Add the caret symbol to omit characters

--- 11_matchSingleUnspecifiedChars.js
+++ 11_matchSingleUnspecifiedChars.js
@@ -1,1 +1,6 @@
 // Match Single Characters Not Specified
+/* The ^ (caret character) is used to omit characters
+and should prefix the undesired characters */
+/*
+/[^aeiou]/gi will match all characters that are not a vowel
+*/
