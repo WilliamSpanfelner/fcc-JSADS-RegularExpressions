7b0f811e52ab92ef2cd9e3b8f6e4bf440ac08b0e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:20:56 +0300

    Use ^ to find leading characters

--- 16_matchBeginningStringPatterns.js
+++ 16_matchBeginningStringPatterns.js
@@ -1,1 +1,11 @@
 // Match Beginning String Patterns
+// A caret (^) outside a character set will match leading substrings
+
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+let result = firstRegex.test(firstString);
+console.log(result);  // true
+
+let notfirst = "You can't find Ricky now.";
+result = firstRegex.test(notfirst);
+console.log(result);  // false
