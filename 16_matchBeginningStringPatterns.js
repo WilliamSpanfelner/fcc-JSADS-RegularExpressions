dff56aa09a1b78e2bbf0ad4b599ee6db7b80cabe
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:25:10 +0300

    Challenge - solution

--- 16_matchBeginningStringPatterns.js
+++ 16_matchBeginningStringPatterns.js
@@ -13,5 +13,7 @@ console.log(result);  // false
 /* Use the caret character in a regex to find Cal only in the 
 beginning of the string rickyAndCal */
 let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /change/;  // Change this line
+let calRegex = /^Cal/;  // Change this line
 result = calRegex.test(rickyAndCal);
+
+console.log(result);
