5562fd638a6f7f68fb1839fe0ab08648affec767
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 20:42:17 +0300

    Update 11_matchSingleUnspecifiedChars.js

--- 11_matchSingleUnspecifiedChars.js
+++ 11_matchSingleUnspecifiedChars.js
@@ -12,4 +12,11 @@ flags in the regex. */
 let quoteSample = "3 blind mice.";
 let myRegex = /[^0-9aeiou]/gi; // Change this line
 let result = quoteSample.match(myRegex);  // Change this line
-console.log(result);
+console.log(result);  
+/* 
+[
+  ' ', 'b', 'l',
+  'n', 'd', ' ',
+  'm', 'c', '.'
+]
+*/
