338c809401d51252a2626eb8f09ad3accf89ce2e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 10:57:19 +0300

    Suffix characters with + to match multiple times

--- 12_matchCharactersOccurringMultipleTimes.js
+++ 12_matchCharactersOccurringMultipleTimes.js
@@ -1,1 +1,7 @@
 // Match Characters that Occur One or More Times
+/* Use the plus (+) character to suffix characters that 
+need matching multiple times. */
+
+/*
+/a+/g would return ['a', 'a'] from a string of abab
+*/
