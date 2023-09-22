46784a7b1f91cefb467a24992b2ead90d6380fca
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:31:51 +0300

    Use $ to match string suffix

--- 17_matchSuffixPatterns.js
+++ 17_matchSuffixPatterns.js
@@ -1,1 +1,10 @@
 // Match Ending String Patterns
+// Match string suffixes with the dollar sign ($) character
+
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+let result = storyRegex.test(theEnding);
+console.log(result);  // true
+let noEnding = "Sometimes a story will have to end";
+result = storyRegex.test(noEnding);
+console.log(result);  // false
