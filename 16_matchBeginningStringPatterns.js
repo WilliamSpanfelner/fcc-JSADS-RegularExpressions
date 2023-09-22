ca3a7e3692bae5a5ea5d7c7488ee6fb57f913dbb
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-22 12:23:51 +0300

    Challenge - start

--- 16_matchBeginningStringPatterns.js
+++ 16_matchBeginningStringPatterns.js
@@ -9,3 +9,9 @@ console.log(result);  // true
 let notfirst = "You can't find Ricky now.";
 result = firstRegex.test(notfirst);
 console.log(result);  // false
+
+/* Use the caret character in a regex to find Cal only in the 
+beginning of the string rickyAndCal */
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /change/;  // Change this line
+result = calRegex.test(rickyAndCal);
