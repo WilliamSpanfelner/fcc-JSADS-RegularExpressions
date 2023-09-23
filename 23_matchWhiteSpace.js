8ab678c68b6643e1b401c3b7748a1ad74aa058e5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-23 10:59:21 +0300

    Use \s to match whitespace

--- 23_matchWhiteSpace.js
+++ 23_matchWhiteSpace.js
@@ -1,1 +1,11 @@
 // Match Whitespace
+/* The \s character class matches whitespace in a string
+that can be composed of whitespace, carriage return, tab, 
+form feed, and new line characters (i.e. [ \r\t\f\n\v] 
+respectively). */
+
+let whitespace = "Whitespace. Whitespace everywhere!";
+let spaceRegex = /\s/g;
+let result = whitespace.match(spaceRegex);
+
+console.log(result);  // [ ' ', ' ' ]
