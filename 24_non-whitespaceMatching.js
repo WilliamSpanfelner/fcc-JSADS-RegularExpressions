b67d65e0c498bc212d6da227591215f365ecc2a3
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-23 11:18:27 +0300

    Use \S to match everything except whitespace

--- 24_non-whitespaceMatching.js
+++ 24_non-whitespaceMatching.js
@@ -1,1 +1,11 @@
 // Match Non-Whitespace Characters
+/* Match everything except for whitespace characters
+with \S which is equivalent to [^ \r\t\f\n\v] as it 
+doesn't match whitespace, carriage return, tab, form feed,
+and new line characters. */
+
+let whitespace = "Whitespace. Whitespace everywhere.";
+let nonSpaceRegex = /\S/g;
+let result = whitespace.match(nonSpaceRegex).length;
+
+console.log(result);  // 32
