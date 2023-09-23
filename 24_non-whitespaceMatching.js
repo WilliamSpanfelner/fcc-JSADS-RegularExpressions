0409cdf31ae9d36ae2a8641e04206c6d18b71c12
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-23 11:24:12 +0300

    Challenge - solution

--- 24_non-whitespaceMatching.js
+++ 24_non-whitespaceMatching.js
@@ -14,5 +14,17 @@ console.log(result);  // 32
 multiple non-whitespace characters in a String. */
 
 let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /change/; //Change this line
+let countNonWhiteSpace = /\S/g; //Change this line
 result = sample.match(countNonWhiteSpace);
+
+console.log(result);  /* 
+  [
+    'W', 'h', 'i', 't', 'e', 's', 'p',
+    'a', 'c', 'e', 'i', 's', 'i', 'm',
+    'p', 'o', 'r', 't', 'a', 'n', 't',
+    'i', 'n', 's', 'e', 'p', 'a', 'r',
+    'a', 't', 'i', 'n', 'g', 'w', 'o',
+    'r', 'd', 's'
+  ]
+
+  */
