dcb4a37205ccb1df5fc5db4687bbc39b434beb5e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 13:47:21 +0300

    Lookahead match

--- 29_Pos&NegLookahead.js
+++ 29_Pos&NegLookahead.js
@@ -1,1 +1,12 @@
 // Positive and Negative Lookahead
+/* A lookahead will look for the presence or not 
+of a character */
+
+let quit = "qu";
+let noquit = 'qt';
+let quRegex = /q(?=u)/;
+let qRegex = /q(?!u)/;
+let result = quit.match(quRegex);
+console.log(result);  // [ 'q' ]
+result = noquit.match(qRegex);
+console.log(result);  // [ 'q' ]
