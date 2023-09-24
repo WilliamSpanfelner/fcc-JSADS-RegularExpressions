f99737e69c6188b93079729a1f2f564cf63ff83a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 13:28:16 +0300

    Check for optional character

--- 28_allOrNoneCheck.js
+++ 28_allOrNoneCheck.js
@@ -1,1 +1,13 @@
 // Check for All or None
+/* Suffixing a regex element with a question mark
+(?) will check for zero or one character. The question
+mark can be thought of as indicating an element as
+optional. */
+
+let american = 'color';
+let british = 'colour';
+let rainbowRegex = /colou?r/;
+let result = rainbowRegex.test(american);
+console.log(result);  // true
+result = rainbowRegex.test(british);
+console.log(result);  // true
