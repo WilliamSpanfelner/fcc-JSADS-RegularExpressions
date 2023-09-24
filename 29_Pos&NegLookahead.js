2a30be671ef2f822e700b8fa5012a54cd6d4f4c8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 14:39:36 +0300

    Challenge - solution

--- 29_Pos&NegLookahead.js
+++ 29_Pos&NegLookahead.js
@@ -13,14 +13,19 @@ console.log(result);  // [ 'q' ]
 
 // Check a password with lookahead
 let password = "abc123"
-let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;  
+//check pw is between 3 and 6 characters and contains at least one number
 result = checkPass.test(password);
-console.log(result);
+console.log(result);  // true
 
 /* Use lookaheads in the pwRegex to match passwords that 
 are greater than 5 characters long, and have two 
 consecutive digits. */
 
-let sampleWord = "astronaut";
-let pwRegex = /change/; Change this AudioListener
+sampleWords = ['astronaut', 'bana12', 'abc123', '8pass99', 'astr1on11aut']
+
+let sampleWord = sampleWords[0];
+let pwRegex = /(?=\w{6,})(?=\D*\d{2})/;  // Change this AudioListener
 result = pwRegex.test(sampleWord);
+
+console.log(sampleWord, result, sampleWord.match(pwRegex));
