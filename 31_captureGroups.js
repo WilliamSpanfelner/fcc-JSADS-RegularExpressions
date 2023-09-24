f6f1ad229410aff9edcbbbb49812b064920d57a1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 15:45:53 +0300

    Add capture group and reuse in regex

--- 31_captureGroups.js
+++ 31_captureGroups.js
@@ -1,1 +1,13 @@
 // Reuse Patterns Using Capture Groups
+/* Capture groups are regex patterns enclosed with 
+parentheses.  Once a group is define in a regex, it 
+can be reference almost as if it was an element of 
+an array - \1 for the first group defined, and so on. */
+
+let repeatedStr = "row row row your boat";
+let repeatRegex = /(\w+) \1 \1/;
+let result = repeatRegex.test(repeatedStr);
+console.log(result);
+
+result = repeatedStr.match(repeatRegex);
+console.log(result);
