cb1adaa4c1c972ba0db73bf0f7a96a7f8bbacb03
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 15:23:17 +0300

    Challenge - solution

--- 30_mixedGroupChecks.js
+++ 30_mixedGroupChecks.js
@@ -7,3 +7,16 @@ let testStr = "Pumpkin";
 let testRegex = /P(engu|umpk)in/;
 let result = testRegex.test(testStr);
 console.log(result);  // true
+
+/* Fix the regex so that it checks fro the names of Franklin
+Roosevelt or Eleanor Roosevelt in a case sensitive manner
+and it should make concessions for middle names.
+
+Then fix the code so that the regex that you have created is 
+checked against myString and either true or false is returned
+depending on whether the regex mathces. */
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Eleanor|Franklin|Franklin D.) Roosevelt/;  // Change this line
+result = myRegex.test(myString);  // Change this line
+console.log(result);
