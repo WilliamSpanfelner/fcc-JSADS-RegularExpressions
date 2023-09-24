4634b7cb9d76fa83fd971f2aeaf124c50de3b5cc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 17:01:22 +0300

    Challenge - start

--- 32_find&Replace.js
+++ 32_find&Replace.js
@@ -11,4 +11,20 @@ console.log(wrongText, result);  // The sky is silver. The sky is blue.
 /* Capture groups can also be used in the first argument
 for the replace method. */
 
-console.log("Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1'));  // Camp Code
+console.log("Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1'));  
+// Camp Code
+
+/* Write a regex fixRegex using three capture groups that
+will search for each word in the string one two three.
+Then update the replaceText variable to replace one
+two three with the string three two one and assign the 
+result to the result variable.  Make sure you are utilizing
+capture groups in the replacement stirng using the dollar
+sign ($) syntax. */
+
+let str = "one two three";
+let fixRegex = /change/;  // Change this line
+let replaceText = "";  // Change this line
+result = str.replace(fixRegex, replaceText);
+
+console.log(result);
