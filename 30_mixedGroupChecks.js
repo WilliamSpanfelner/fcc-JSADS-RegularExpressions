a53a0cc2ed9bdc91024933b9f12a9c1366b0b7e4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 14:47:49 +0300

    Check for char group

--- 30_mixedGroupChecks.js
+++ 30_mixedGroupChecks.js
@@ -1,1 +1,9 @@
 // Check for Mixed Grouping of Characters
+/* Surround groups of characters to be matched with 
+parentheses.  E.g. if you want to find Penguin or Pumpkin
+the regex /P(engu|umpk)in/g could be used. */
+
+let testStr = "Pumpkin";
+let testRegex = /P(engu|umpk)in/;
+let result = testRegex.test(testStr);
+console.log(result);  // true
