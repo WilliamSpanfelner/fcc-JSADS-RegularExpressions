5e2ac93697fd013f51c8f5f27ab07100b9c28511
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 16:50:54 +0300

    Replace text in a string with regex

--- 32_find&Replace.js
+++ 32_find&Replace.js
@@ -1,1 +1,9 @@
 // Use Capture Groups to Search and Replace
+/* The .replace() string method takes a regex to match
+and a string to replace once a match is made */
+
+let wrongText = "The sky is silver.";
+let silverRegex = /silver/;
+let result = wrongText.replace(silverRegex, "blue");
+
+console.log(wrongText, result);
