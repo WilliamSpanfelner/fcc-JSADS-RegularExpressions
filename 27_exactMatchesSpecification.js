829e02a8aec6b2148ce0e77747fd57d4f77cc334
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-24 13:04:42 +0300

    Match x number of times

--- 27_exactMatchesSpecification.js
+++ 27_exactMatchesSpecification.js
@@ -1,1 +1,14 @@
 // Specify Exact Number of Matches
+/* Matching a pattern x number of tines is specified in curly
+brackets */
+
+let a4 = "haaaah";
+let a3 = "haaah";
+let a100 = "h" + "a".repeat(100) + "h";
+let multipleA = /ha{3}h/;
+let result = multipleA.test(a4);
+console.log(result);  // false
+result = multipleA.test(a3);
+console.log(result);  // true
+result = multipleA.test(a100);
+console.log(result);  // false
